212f91c112ae1eae2af3d29ff9b4e622b2913864
Ben Ashford <ben@bcash.org>
2012-08-17 23:36:25 +0100

    Improved description, URL, and version number

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject extra-time "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject extra-time "0.1.0"
+  :description "A library designed to capture execution time in a more granular way than clojure.core's time, but less so than a proper profiling tool."
+  :url "https://github.com/benashford/extra-time"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :main extra-time.core)
