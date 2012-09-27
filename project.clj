c06532ad10a9af7f8881c5a9d8657e50377257c2
Ben Ashford <ben@bcash.org>
2012-09-27 23:12:42 +0100

    Bumped version for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject extra-time "0.1.0"
+(defproject extra-time "0.1.1"
   :description "A library designed to capture execution time in a more granular way than clojure.core's time, but less so than a proper profiling tool."
   :url "https://github.com/benashford/extra-time"
   :dependencies [[org.clojure/clojure "1.4.0"]]
